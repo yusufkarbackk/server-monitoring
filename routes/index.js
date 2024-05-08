26fff3110342ac15a84cdeeda8bace98caffc1bb
unknown <puguhammar05@gmail.com>
2024-05-08 16:29:29 +0700

    Mengambil data dari firebase database realtime

--- routes/index.js
+++ routes/index.js
@@ -2,40 +2,40 @@ const { initializeApp } = require('firebase/app');
 const { getDatabase, ref, set } = require('firebase/database');
 var express = require('express');
 var router = express.Router();
-const admin = require('../db')
+const admin = require('../db');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/get-data', function (req, res) {
-  res.send(
-    [
-      {
-        "name": "27",
-        "description": "30"
-      },
-      {
-        "name": "25",
-        "description": "90"
-      },
-    ]
-  )
-})
+// Endpoint untuk mendapatkan data (belombisa)
+router.get('/get-data', (req, res) => {
+  admin
+    .database()
+    .ref('sensorData')
+    .once('value')
+    .then((snapshot) => {
+      const data = snapshot.val();
+      res.json(data);
+    })
+    .catch((error) => {
+      console.error('The read failed:', error);
+      res.status(500).json({ error: 'Something went wrong' });
+    });
+});
 
 router.post('/post-data', async (req, res) => {
-
   try {
     const { suhu, kelembaban } = req.body;
 
     // Save data to Firebase Realtime Database
-    await admin.database().ref("sensorData").push({ suhu, kelembaban });
+    await admin.database().ref('sensorData').push({ suhu, kelembaban });
 
-    res.status(200).json({ message: "Data saved to Firebase Realtime Database" });
+    res.status(200).json({ message: 'Data saved to Firebase Realtime Database' });
   } catch (error) {
-    console.error("Error saving data to Firebase Realtime Database:", error);
-    res.status(500).json({ error: "Internal server error" });
+    console.error('Error saving data to Firebase Realtime Database:', error);
+    res.status(500).json({ error: 'Internal server error' });
   }
   // const data = req.body
   // console.log('received sensor data', data)
@@ -50,6 +50,8 @@ router.post('/post-data', async (req, res) => {
   //     console.error('Error saving data:', error);
   //     res.status(500).send('Error saving data');
   //   })
-})
+});
+
+//REGISTER USER
 
 module.exports = router;
