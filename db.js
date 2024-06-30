8b4c42aa4546ccc8a31450f919e3b1de64b97d21
Yusuf karback <yusufkarback64@gmail.com>
2024-06-30 15:41:57 +0700

    adding .env file

--- db.js
+++ db.js
@@ -1,8 +1,11 @@
 const admin = require('firebase-admin')
 
-var serviceAccount = require("./service-account-2.json");
+//var serviceAccount = require("./server-monitoring-19773-firebase-adminsdk-9xsse-71a0194911.json");
+// Load environment variables
+require('dotenv').config();
 
-
+// Load the service account key from an environment variable
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
